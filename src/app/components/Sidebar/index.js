defa0d996bdafcaa79ae073bbd2f1e53d7c1e165
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2020-05-02 15:12:18 -0300

    Added logo link

--- src/app/components/Sidebar/index.js
+++ src/app/components/Sidebar/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 import structure from '~constants/structure';
 
 import logo from '~assets/logo-white.png';
+
+import Routes from '~constants/routes';
 
 import styles from './styles.module.scss';
 
 function Sidebar() {
   return (
     <aside className={styles.sidebarContainer}>
-      <img src={logo} alt="logo" className={`${styles.logo} m-top-1 m-bottom-1`} />
+      <Link to={Routes.HOME} className={`${styles.logo} m-top-1 m-bottom-1`}>
+        <img src={logo} alt="logo" className={styles.logo} />
+      </Link>
       {structure
         .filter(model => !model.only || model.only.INDEX)
         .map(model => (
